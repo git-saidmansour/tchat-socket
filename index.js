074d3989c25e91d0864c86780ea3dd4c1a4a00c4
unknown <mansour50said@gmail.com>
2024-06-29 02:12:46 +0100

    Amélioration de la gestion des heures

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+let connectedUsers = [];
+
 app.set("view engine", 'ejs');
 
 app.use(cors())
@@ -24,18 +26,52 @@ app.get('/chat', (req, res) => {
     res.render('chat.ejs', {username: req.query.username});
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on('connection', async (socket) => {
+  try {
+    let userInfo = await socket.timeout(5000).emitWithAck('register event')
+    connectedUsers.push({
+      username: userInfo.username,
+      socketId: socket.id
+    });
+    console.log(userInfo.username, 'is conected');
     
     socket.on('chat message', (message) => {
-        message.messageTime = moment().format("HH:mm");
-        console.log(`${message.senderName}: ${message.message}\n${message.messageTime}`)
-        io.emit('chat message', message);
+        io.timeout(5000).emit('date event', (error, response) => {
+          if (error) {
+            console.log("une erreur");
+            message.messageTime = moment().format("HH:mm");
+            io.emit('chat message', message);
+          } else {
+            console.log("pas d'erreur");
+            for(const elt of response){
+              message.messageTime = moment(elt.date).format("HH:mm");
+              const user = connectedUsers.filter((user) => user.username === elt.username);
+              io.to(user[0].socketId).emit('chat message', message);
+            }
+          }
+        })
     });
 
+    // socket.onAny((eventName, ...args) => {
+    //   console.log("debug in event")
+    //   console.log(eventName); // 'hello'
+    //   console.log(args); // [ 1, '2', { 3: '4', 5: ArrayBuffer (1) [ 6 ] } ]
+    // });
+
+    // socket.onAnyOutgoing((eventName, ...args) => {
+    //   console.log("debug out event")
+    //   console.log(eventName); // 'hello'
+    //   console.log(args); // [ 1, '2', { 3: '4', 5: ArrayBuffer (1) [ 6 ] } ]
+    // });
+
     socket.on('disconnect', () => {
+        connectedUsers = connectedUsers.filter(user => user.socketId !== socket.id);
         console.log('user disconnected');
     });
+  } catch (error) {
+    console.log(error.message)
+    socket.disconnect();
+  }
 });
 
 server.listen(process.env.PORT || '3000', () => {
